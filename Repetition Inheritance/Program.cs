0d11d1f9a678b16203b8fc09a4f8bb36ddb8ff7f
Matias <Matias@DESKTOP-TA0TGEP>
2017-02-08 10:06:51 +0100

    OPgave 8 lavet ny liste kaldet bils over alle de forskellige biler og added prop og metoder til listen.

--- Repetition Inheritance/Program.cs
+++ Repetition Inheritance/Program.cs
@@ -31,27 +31,74 @@ namespace Repetition_Inheritance
             elBils.Add(new ElBil("cheveolet", 200000, 2017, "8Y5C", 10, 23));
             elBils.Add(new ElBil("Tesla", 700000, 2017, "7UP9", 10, 60));
 
-            foreach (DieselBil biler in dieselBils)
-            {
-                Console.WriteLine("Mærke: "  + biler.Mærke  +   "   Bilens pris er: " + biler.BilPrisExAfgift    +    "   Købt: "   + biler.KøbsÅr   +   "    RegistreringNr: "  +  biler.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + biler.HalvÅrligEjerafgift()  + " Rækkevidde: " + biler.Rækkevidde());
-            }
+            List<Bil> bils = new List<Bil>();
+            bils.Add(new DieselBil("BMW", 200000, 2006, 10, "23r23r", 69));
+            bils.Add(new DieselBil("Porche", 800000, 2015, 32, "hewh45h", 7));
+            bils.Add(new DieselBil("Skoda", 300000, 2004, 20, "w4h4h5h4", 6, false));
+            bils.Add(new DieselBil("Kia", 1500000, 2013, 15, "jt6tyrj", 10));
+            bils.Add(new DieselBil("Lamboghini", 2000000, 2014, 26, "gw54g4g", 8, false));
+            bils.Add(new BenzinBil("BMW", 200000, 2004, 10, "q3t87gh", 9));
+            bils.Add(new BenzinBil("Porche", 240000, 2004, 20, "b8um5r", 3));
+            bils.Add(new BenzinBil("Skoda", 80000, 2004, 29, "mvw9wv4", 122));
+            bils.Add(new BenzinBil("Kia", 740000, 2004, 15, "9wmvw4v", 8));
+            bils.Add(new BenzinBil("Lamboghini", 1400000, 2004, 9, "a9je4rv", 5));
+            bils.Add(new ElBil("BMW", 250000, 2010, "3D2S", 10, 20));
+            bils.Add(new ElBil("Nissan", 170000, 2017, "4D2W", 5, 50));
+            bils.Add(new ElBil("Toyota", 150000, 2017, "9F4T", 5, 15));
+            bils.Add(new ElBil("cheveolet", 200000, 2017, "8Y5C", 10, 23));
+            bils.Add(new ElBil("Tesla", 700000, 2017, "7UP9", 10, 60));
 
-            Console.WriteLine("==========================");
+            //foreach (DieselBil biler in dieselBils)
+            //{
+            //    Console.WriteLine("Mærke: "  + biler.Mærke  +   "   Bilens pris er: " + biler.BilPrisExAfgift    +    "   Købt: "   + biler.KøbsÅr   +   "    RegistreringNr: "  +  biler.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + biler.HalvÅrligEjerafgift()  + " Rækkevidde: " + biler.Rækkevidde());
+            //}
+
+            //Console.WriteLine("==========================");
             
-            foreach (BenzinBil benzin in benzinBils)
+            //foreach (BenzinBil benzin in benzinBils)
+            //{
+            //    Console.WriteLine("Mærke: "  + benzin.Mærke  +   "   Bilens pris er: " + benzin.BilPrisExAfgift    +    "   Købt: "   + benzin.KøbsÅr   +   "    RegistreringNr: "  +  benzin.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + benzin.HalvÅrligEjerafgift()  + " Rækkevidde: " + benzin.Rækkevidde());
+
+            //}
+
+            //Console.WriteLine("===============================");
+
+            //foreach (ElBil elBil in elBils)
+            //{
+            //    Console.WriteLine("Mærke: "  + elBil.Mærke  +   "   Bilens pris er: " + elBil.BilPrisExAfgift    +    "   Købt: "   + elBil.KøbsÅr   +   "    RegistreringNr: "  +  elBil.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + elBil.HalvÅrligEjerafgift()  + " Rækkevidde: " + elBil.Rækkevidde());
+
+            //}
+            //Console.ReadLine();
+
+            foreach (Bil bil in bils)
             {
-                Console.WriteLine("Mærke: "  + benzin.Mærke  +   "   Bilens pris er: " + benzin.BilPrisExAfgift    +    "   Købt: "   + benzin.KøbsÅr   +   "    RegistreringNr: "  +  benzin.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + benzin.HalvÅrligEjerafgift()  + " Rækkevidde: " + benzin.Rækkevidde());
+                if (bil is ElBil)
+                {
+                    ElBil elBil = bil as ElBil;
+                    Console.WriteLine("Mærke: "  + elBil.Mærke  +   "   Bilens pris er: " + elBil.BilPrisExAfgift    +    "   Købt: "   + elBil.KøbsÅr   +   "    RegistreringNr: "  +  elBil.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + elBil.HalvÅrligEjerafgift()  + " Rækkevidde: " + elBil.Rækkevidde());
 
+                }
+
+                if (bil is DieselBil)
+                {
+                    DieselBil biler = bil as DieselBil;
+                    Console.WriteLine("Mærke: "  + biler.Mærke  +   "   Bilens pris er: " + biler.BilPrisExAfgift    +    "   Købt: "   + biler.KøbsÅr   +   "    RegistreringNr: "  +  biler.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + biler.HalvÅrligEjerafgift()  + " Rækkevidde: " + biler.Rækkevidde());
+
+                }
+
+                if (bil is BenzinBil)
+                {
+                    BenzinBil benzin = bil as BenzinBil;
+                    Console.WriteLine("Mærke: "  + benzin.Mærke  +   "   Bilens pris er: " + benzin.BilPrisExAfgift    +    "   Købt: "   + benzin.KøbsÅr   +   "    RegistreringNr: "  +  benzin.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + benzin.HalvÅrligEjerafgift()  + " Rækkevidde: " + benzin.Rækkevidde());
+                   
+                }
+                {
+                    
+                }
+                {
+                    
+                }
             }
-
-            Console.WriteLine("===============================");
-
-            foreach (ElBil elBil in elBils)
-            {
-                Console.WriteLine("Mærke: "  + elBil.Mærke  +   "   Bilens pris er: " + elBil.BilPrisExAfgift    +    "   Købt: "   + elBil.KøbsÅr   +   "    RegistreringNr: "  +  elBil.RegistreringsNr + "     Den halvårlige ejerafgift er: "  + elBil.HalvÅrligEjerafgift()  + " Rækkevidde: " + elBil.Rækkevidde());
-
-            }
-            Console.ReadLine();
         }
     }
 }
